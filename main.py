8d1d0ad7e4afcc6f9ab25d4dff898b59792134ff
PabloSalinas2 <gesalinas.b@gmail.com>
2023-12-31 17:13:28 -0300

    main actualizado

--- main.py
+++ main.py
@@ -14,29 +14,7 @@ df_funcion3_4=pd.read_csv('Datos_procesados/funcion3y4.csv')
 df_funcion5=pd.read_csv('Datos_procesados/funcion5.csv')
 matriz_user_item=pd.read_parquet('Datos_procesados/matriz_user-item.parquet')
 nombre_juegos=pd.read_parquet('Datos_procesados/nombre_juegos.parquet')
-
-# Lectura del archivo "matriz_utiilidad.parquet"
-# Tamaño del chunk
-chunksize = 100
-# Ruta al archivo Parquet
-parquet_file_path = 'Datos_procesados/matriz_utilidad.parquet'
-# Configurar el lector Parquet
-parquet_file = pq.ParquetFile(parquet_file_path)
-num_rows = parquet_file.num_row_groups
-# Iterar sobre los chunks
-for i in range(0, num_rows, chunksize):
-    # Calcular el rango de filas para el chunk actual
-    start_row = i
-    end_row = min(i + chunksize, num_rows)
-    # Leer el chunk
-    table = pq.read_table(parquet_file_path)# row_groups=list(range(start_row, end_row)))
-    # Convertir la tabla a un DataFrame de Pandas
-    matriz_utilidad= table.to_pandas()
-
-
-
-
-    #matriz_utilidad=pd.read_parquet('Datos_procesados/matriz_utilidad.parquet')
+#matriz_utilidad=pd.read_parquet('Datos_procesados/matriz_utilidad.parquet')
 
 app= FastAPI()
 app.title='Steam Games: Querys'
@@ -53,7 +31,7 @@ def PlayTimeGenre( genero : str ): #  Debe devolver año con mas horas jugadas p
     except Exception as e:
         return {'Genero incorrecto'}
 
-#@profile
+# @profile
 @app.get("/user_for_genre/{genero}")
 def UserForGenre( genero : str ): #Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
     try:
@@ -78,7 +56,7 @@ def UserForGenre( genero : str ): #Debe devolver el usuario que acumula más hor
 # Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
 
 
-#@profile
+# @profile
 @app.get("/users_recommend/{anio}")
 def UsersRecommend( year : int ): # Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
     try:
@@ -91,7 +69,7 @@ def UsersRecommend( year : int ): # Devuelve el top 3 de juegos MÁS recomendado
 #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
     
 
-#@profile
+# @profile
 @app.get("/users_not_recommend/{anio}")
 def UsersNotRecommend( year : int ): # Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
     try:
@@ -105,7 +83,7 @@ def UsersNotRecommend( year : int ): # Devuelve el top 3 de juegos MENOS recomen
 
 
 
-#@profile
+# @profile
 @app.get("/sentiment_analysis/{anio}")
 def sentiment_analysis( year : int ): # Según el año de lanzamiento, se devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento.
     positivos=0
@@ -123,7 +101,7 @@ def sentiment_analysis( year : int ): # Según el año de lanzamiento, se devuel
 
 # Ejemplo de retorno: {Negative = 182, Neutral = 120, Positive = 278}
 
-#@profile
+# @profile
 @app.get("/recommend_system/{user}")
 def sistema_recomendacion(usuario_objetivo):
     # Lectura parcial de matriz utilidad 
